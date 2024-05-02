9c33e2f24f37892e0178b23ff7308ea87c5a1dea
rollenasistores <asistoresrlc1@gmail.com>
2024-05-02 09:02:44 +0800

    removed to console and I use app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Mail\ReturnBookEmail;
+use App\Models\Borrow;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\Admin;
 use App\Http\Middleware\Student;
+use Illuminate\Support\Facades\Mail;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -22,6 +26,18 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         //
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->call(function () {
+            $borrows = Borrow::with('user')->where('confirmed', 0)
+            ->whereDate('returned_at', today())
+            ->get();
+        
+            foreach ($borrows as $borrow) {
+        
+                Mail::to($borrow->user['email'])->send(new ReturnBookEmail($borrow->user['name'], $borrow->returned_at, $borrow->copy->book->name));
+            }
+        })->twiceDaily(8, 13);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/console.php
+++ routes/console.php
@@ -1,25 +1,9 @@
 <?php
 
-use App\Mail\ReturnBookEmail;
-use App\Models\Borrow;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::call(function () {
-
-    $borrows = Borrow::with('user')->where('confirmed', 0)
-    ->whereDate('returned_at', today())
-    ->get();
-
-    foreach ($borrows as $borrow) {
-
-        Mail::to($borrow->user['email'])->send(new ReturnBookEmail($borrow->user['name'], $borrow->returned_at, $borrow->copy->book->name));
-    }
-
-})->twiceDaily(8, 13);
