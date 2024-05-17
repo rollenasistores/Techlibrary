6b2db9d3019f3a0a30b05755555e1dd05b5d4014
rollenasistores <asistoresrlc1@gmail.com>
2024-05-18 05:57:27 +0800

    fix

--- resources/js/Pages/Books.vue
+++ resources/js/Pages/Books.vue
@@ -107,7 +107,7 @@ export default {
                         </div>
                         <div class="mt-4">
                             <span v-if="countAvailableCopies(book) > 0"
-                                class="py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium bg-custom-blue text-custom-blue rounded-full dark:bg-red-500/10 dark:text-custom-blue">
+                                class="py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium bg-custom-blue text-white rounded-full dark:bg-red-500/10 dark:text-custom-blue">
                                 <svg class="flex-shrink-0 size-3" xmlns="http://www.w3.org/2000/svg" width="24"
                                     height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                     stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
